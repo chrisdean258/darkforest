c69d45b8e65414f63ccf4675bc1e8660dfccf015
chrisdean258 <chrisdean258@gmail.com>
2018-01-20 03:07:26 -0500

    allowing a spot for putting the maze in the php file along with rendering maze

--- webApp/game/game.js
+++ webApp/game/game.js
@@ -1,18 +1,23 @@
 var canvas;
 var person;
 var maze;
+var blockSize = 20;
 
 function setup()
 {
-	canvas = createCanvas(600, 600);
+	canvas = createCanvas(boardWidth * blockSize, boardHeight * blockSize);
 	canvas.parent("#canvasparentdiv");
 
 	background(0,0,0);
 
-	person = new block(100, 100, 100);
+	person = new block(200, 20, 20);
 	person.show();
 
 	maze = getMaze();
+	for(var i = 0; i < maze.length; i++)
+	{
+		maze[i].show();
+	}
 
 }
 
@@ -23,5 +28,14 @@ function draw()
 
 function getMaze()
 {
-
+	var i, j;
+	var rtn = [];
+	for(i = 0; i < maze_xs.length; i++)
+	{
+		for(j = 0; j < maze_ys.length; j++)
+		{
+			rtn.push(new block(maze_xs[i] * blockSize, maze_ys[i] * blockSize, blockSize, "#888888"));
+		}
+	}
+	return rtn;
 }
