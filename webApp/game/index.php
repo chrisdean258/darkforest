fd1700ca21b0a68c072fbad21a12be9725c99b46
Brent Hurst <seqvalley77@aol.com>
2018-01-20 14:56:28 -0500

    same file

--- webApp/game/index.php
+++ webApp/game/index.php
@@ -20,7 +20,41 @@
 		<script src="person.js"></script>
 		<script src="game.js"></script>
 		<script src="callbacks.js"></script>
+<script>
 
+var arr = []
+
+function selectCallbackMove()
+{
+	arr.push(document.getElementByID("moveSelect").value);
+}
+function selectCallbackIf()
+{
+	arr.push(document.getElementByID("ifSelect").value);
+}
+function selectCallbackWhile()
+{
+	arr.push(document.getElementByID("whileSelect").value);
+}
+function selectCallbackFor()
+{
+	console.log("Callback for");
+	if(this.initialvar == undefined) this.initialvar = [ "k" ];
+	var lc = document.getElementByID("forSelect").value;
+	var lv = this.initialvar.join("");
+
+	arr.push("for(var " + lv + " = 0; " + lv + " < " + lc + " ; " + lv + "++){");
+
+	this.initialvar.push("j");
+}
+function selectCallbackDone()
+{
+	arr.push(document.getElementByID("doneSelect").value);
+	for(var i = 0; i<arr.length; i++)
+		console.log(arr[i]);
+}
+
+</script>
 </head>
 <body>
 
