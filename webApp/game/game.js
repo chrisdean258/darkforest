0db230e383c6a032c8096cae232524ca7a2a9ffa
chrisdean258 <chrisdean258@gmail.com>
2018-01-20 06:07:28 -0500

    path image added

--- webApp/game/game.js
+++ webApp/game/game.js
@@ -8,7 +8,7 @@ function preload()
 {
 	board = [];
 	for(var i = 0; i < boardHeight; i++) board.push([]);
-	person = new Person(0, boardHeight-1, blockSize, "friend.png");
+	person = new Person(0, boardHeight-1, blockSize, "imgs/friend.png");
 }
 
 function setup()
@@ -23,21 +23,13 @@ function setup()
 	{
 		for(var j = 0; j < boardWidth; j++)
 		{
-			board[i].push(new Block(i, j, blockSize));
+			board[i].push(new Block(i, j, blockSize, "imgs/not_path.png"));
 		}
 	}
 
 	for(var i = 0; i < maze_xs.length; i++)
 	{
-		board[maze_xs[i]][maze_ys[i]].color = "#888888";
-	}
-
-	for(var i = 0; i < board.length; i++)
-	{
-		for(var j = 0; j < boardWidth; j++)
-		{
-			board[i][j].show();
-		}
+		board[maze_xs[i]][maze_ys[i]].image = loadImage("imgs/path.png");
 	}
 
 	board[person.x][person.y-1].visible = true;
@@ -45,10 +37,9 @@ function setup()
 	board[person.x+1][person.y].visible = true;
 	board[person.x][person.y].visible = true;
 
-	board[boardWidth-1][0].color = "#355c3d";
+	//board[boardWidth-1][0].color = "#355c3d";
 	board[boardWidth-1][0].visible = true;
 
-	person.show();
 }
 
 function draw()
