9cf96e3b147e2d21aa7aab2a6a7b444e20663283
chrisdean258 <chrisdean258@gmail.com>
2018-01-20 07:54:49 -0500

    Update to old version game.js

--- webApp/game/game.js
+++ webApp/game/game.js
@@ -8,7 +8,7 @@ function preload()
 {
 	board = [];
 	for(var i = 0; i < boardHeight; i++) board.push([]);
-	person = new Person(0, boardHeight-1, blockSize, "friend.png");
+	person = new Person(0, boardHeight-1, blockSize, "imgs/friend.png");
 }
 
 function setup()
@@ -23,21 +23,21 @@ function setup()
 	{
 		for(var j = 0; j < boardWidth; j++)
 		{
-			board[i].push(new Block(i, j, blockSize));
+			if(random() > .1)
+			{
+				board[i].push(new Block(i, j, blockSize, "imgs/not_path.png"));
+			}
+			else
+			{
+				board[i].push(new Block(i, j, blockSize, "imgs/not_path_fun.png"));
+			}
 		}
 	}
 
 	for(var i = 0; i < maze_xs.length; i++)
 	{
-		board[maze_xs[i]][maze_ys[i]].color = "#888888";
-	}
-
-	for(var i = 0; i < board.length; i++)
-	{
-		for(var j = 0; j < boardWidth; j++)
-		{
-			board[i][j].show();
-		}
+		board[maze_xs[i]][maze_ys[i]].image = loadImage("imgs/path.png");
+		board[maze_xs[i]][maze_ys[i]].isPath = true;
 	}
 
 	board[person.x][person.y-1].visible = true;
@@ -45,10 +45,9 @@ function setup()
 	board[person.x+1][person.y].visible = true;
 	board[person.x][person.y].visible = true;
 
-	board[boardWidth-1][0].color = "#355c3d";
+	//board[boardWidth-1][0].color = "#355c3d";
 	board[boardWidth-1][0].visible = true;
 
-	person.show();
 }
 
 function draw()
@@ -62,4 +61,16 @@ function draw()
 	}
 
 	person.show();
+}
+
+function revealAll()
+{
+	for(var i = 0; i < 30; i++)
+	{
+		for(var j = 0; j < 30; j++)
+		{
+			board[i][j].visible = true;
+		}
+	}
+
 }
