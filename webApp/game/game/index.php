14e1531783804178396e351301b8cb0418606032
colsarcol <git@cmawhinney.com>
2018-01-20 06:18:11 -0600

    Updated index.php to the template and started two divs for game info

---
+++ webApp/game/game/index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>dark forest: a programming adventure</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="../working/css/darkgame.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.6.0/p5.js"></script>
+		<script>
+			var maze_xs = [ 1, 2, 3, 4, 5, 6, 7 ];
+			var maze_ys = [ 1, 2, 3, 4, 5, 6, 7 ];
+			var boardWidth = 30;
+			var boardHeight = 30;
+		</script>
+		<script src="block.js"></script>
+		<script src="person.js"></script>
+		<script src="game.js"></script>
+
+</head>
+<body>
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#">dark forest</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">How to play</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Stats</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<br>
+  <div id="wrapper">
+    <div id="leftcolumn">
+      <div id="canvasparentdiv"></div>
+    </div>
+    <div id="rightcolumn">
+       <center> Right</center>
+    </div>
+</div>
+</body>
+</html>
+
