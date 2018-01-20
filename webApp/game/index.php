814088b614f9537210b9c1c442938df28ecee471
colsarcol <git@cmawhinney.com>
2018-01-20 09:20:21 -0600

    added the php to call mazegen c++ program and echo the vars for JS to create map

--- webApp/game/index.php
+++ webApp/game/index.php
@@ -11,10 +11,13 @@
 
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.6.0/p5.js"></script>
 		<script>
-			var maze_xs = [ 1, 2, 3, 4, 5, 6, 7 ];
-			var maze_ys = [ 1, 2, 3, 4, 5, 6, 7 ];
-			var boardWidth = 30;
-			var boardHeight = 30;
+			<?php
+			echo exec("../mazegen/mazegen 30 30");
+			?>
+			//var maze_xs = [ 1, 2, 3, 4, 5, 6, 7 ];
+			//var maze_ys = [ 1, 2, 3, 4, 5, 6, 7 ];
+			//var boardWidth = 30;
+			//var boardHeight = 30;
 		</script>
 		<script src="block.js"></script>
 		<script src="person.js"></script>
