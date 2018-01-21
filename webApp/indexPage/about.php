074a226a7ba7e8e5c848c39ffb7ac289c32c983e
colsarcol <git@cmawhinney.com>
2018-01-21 08:13:50 -0600

    Adding about page

---
+++ webApp/indexPage/about.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>how to play dark forest</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/darkhowto.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="index.php">dark forest</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="about.php">About</a></li>
+        <li><a href="howto.php">How to play</a></li>
+        <li><a href="contact.php">Contact</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<div class="align-left">
+  <h2>About Us</h2>
+</div>
+
+<div class="align-left">
+	<p><br><br>
+	We are the Low Gravity Hack Lads, a super excited group of students from The University of Tennessee, Knoxville. We are all majoring in computer science, during our time with the University we have had the great opportunity to learn more about our amazing field and want to share that knowledge with the next generation of students. 
+<br><br>
+Dark Forest is a simple to use web application that is targeted at younger students who might be interested in technology, science or Engineering. Our goal is to help foster and inspire the imaginations of children while helping teach them skills that they can apply to their trade one day. 
+<br><br>
+Our application helps teach the basic concepts of programming logic and allows the student to visualize the actions they have taken. The game is simple and consists of a maze at the lower right corner of the screen and you must escalope at the upper right. The map will initially be cover with black until you start to explore the corners of the forest. During this time, you will control the actions of the main character by selecting basic programming terms and conditions that will guide you through the maze; and hopefully into your future as a programmer. 
+
+	</p>
+</div>
+
+</body>
+</html>
+
